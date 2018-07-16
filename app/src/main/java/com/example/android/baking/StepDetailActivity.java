d0b575ec16d257a0adc6aa6e18cb985e50508c84
Zukka <info@zukkadev.net>
2018-07-16 22:56:19 +0200

    Add empty step detail activity

---
+++ app/src/main/java/com/example/android/baking/StepDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.baking;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class StepDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_step_detail);
+    }
+}
